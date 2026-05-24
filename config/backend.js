495d17e98a7b0c09444f5b741859eb0e598a5a23
Your Name <you@example.com>
2026-05-24 16:41:00 +0530

    chnaged backedn url

--- config/backend.js
+++ config/backend.js
@@ -1,5 +1,5 @@
 // Central backend configuration
 // Change this URL to update the backend for the entire application
 // export const BACKEND_URL = "https://bgv.tfgverify.com";
-// export const BACKEND_URL = "https://eulalia-indeciduous-danyell.ngrok-free.dev";
- export const BACKEND_URL = "https://hrms-bgv.onrender.com";
+ export const BACKEND_URL = "https://eulalia-indeciduous-danyell.ngrok-free.dev";
+// export const BACKEND_URL = "https://hrms-bgv.onrender.com";
